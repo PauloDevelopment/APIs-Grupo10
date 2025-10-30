2a24144f1e9a668b117ed8b2a1035ab0d51a6889
Paulo Henrique <ppirescordeiro2017@gmail.com>
2025-10-30 10:44:32 -0300

    Correção de rota do Swagger, implementação do CORS e formatação em UTF-8 de retorno JSON

--- API Monolítica - AP1/servico_reservas/app.py
+++ API Monolítica - AP1/servico_reservas/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flasgger import Swagger
 from config import Config
 from models import db
+from flask_cors import CORS
 
 from controllers.reserva_controller import reserva_bp
 
@@ -10,6 +11,14 @@ app.config['JSON_AS_ASCII'] = False
 app.config.from_object(Config)
 
 db.init_app(app)
+
+CORS(app)
+
+@app.after_request
+def set_utf8_charset(response):
+    if response.mimetype == 'application/json':
+        response.headers['Content-Type'] = 'application/json; charset=utf-8'
+    return response
 
 swagger = Swagger(app, template_file='swagger.json')
 
